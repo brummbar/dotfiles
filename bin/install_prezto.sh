246b3cd856b297a841d0e8875de3c48ec2909ca0
Francesco Sardara <francesco.sardara@kanooh.be>
2015-05-17 23:45:15 +0200

    installation procedure based on amazing alrra/dotfiles

---
+++ bin/install_prezto.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE}")" && source "utils.sh"
+
+declare -a FILES_TO_SYMLINK=(
+    "zlogin"
+    "zlogout"
+    "zpreztorc"
+    "zprofile"
+    "zshenv"
+    "zshrc"
+)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    declare -r BASE16_DIR="$HOME/.base16"
+
+    local i=""
+    local sourceFile=""
+    local targetFile=""
+
+    # Check if `Git` is installed
+    if [ $(cmd_exists "git") -eq 1 ]; then
+        print_error "Git is required, please install it!\n"
+        exit 1
+    fi
+
+    # Clone base16-vim
+    if [ ! -d "$BASE16_DIR/base16-vim" ]; then
+
+        git clone https://github.com/chriskempson/base16-vim.git "$BASE16_DIR/base16-vim" &> /dev/null
+        print_result $? "Clone base16-vim" "true"
+
+    fi
+
+    # Clone base16-shell
+    if [ ! -d "$BASE16_DIR/base16-shell" ]; then
+
+        git clone https://github.com/chriskempson/base16-shell.git "$BASE16_DIR/base16-shell" &> /dev/null
+        print_result $? "Clone base16-shell" "true"
+
+    fi
+
+    # Clone Brummbar prezto
+    if [ ! -d "${ZDOTDIR:-$HOME}/.zprezto" ]; then
+
+        git clone --recursive https://github.com/brummbar/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+        print_result $? "Clone Brummbar prezto" "true"
+
+    fi
+
+
+    # Create symlinks for prezto
+    for i in ${FILES_TO_SYMLINK[@]}; do
+
+        sourceFile="${ZDOTDIR:-$HOME}/.zprezto/runcoms/$i"
+        targetFile="${ZDOTDIR:-$HOME}/.$i"
+
+        if [ -e "$targetFile" ]; then
+            if [ "$(readlink "$targetFile")" != "$sourceFile" ]; then
+
+                ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
+                if answer_is_yes; then
+                    execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+                else
+                    print_error "$targetFile → $sourceFile"
+                fi
+
+            else
+                print_success "$targetFile → $sourceFile"
+            fi
+        else
+            execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+        fi
+
+    done
+
+    # Symlink the matching vim color
+    mkdir -p "$HOME/.vim/colors"
+    sourceFile="$BASE16_DIR/base16-vim/colors/base16-tomorrow.vim"
+    targetFile="$HOME/.vim/colors/base16-tomorrow.vim"
+    execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+
+}
+
+main
---
+++ dotfiles.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+# ----------------------------------------------------------------------
+# | Helper Functions                                                   |
+# ----------------------------------------------------------------------
+
+check_os() {
+
+    declare -r OS_NAME="$(uname -s)"
+
+    # Check if the OS is OS X and it is above a certain version
+    if [ "$OS_NAME" == "Darwin" ]; then
+        if [ $(compare_versions "$(sw_vers -productVersion)" "10.10") == "<" ]; then
+            printf "Sorry, this script is intended only for OS X 10.10.0+."
+            return 1
+        else
+            return 0
+        fi
+
+    # Check if the OS is Ubuntu
+    else
+        if [ "$OS_NAME" != "Linux" ] || [ ! -e "/etc/lsb-release" ]; then
+            printf "Sorry, this script is intended only for OS X and Ubuntu!"
+            return 1
+        else
+            return 0
+        fi
+    fi
+
+}
+
+compare_versions() {
+
+    declare -a v1=(${1//./ })
+    declare -a v2=(${2//./ })
+    local i=""
+
+    # Fill empty positions in v1 with zeros
+    for (( i=${#v1[@]}; i<${#v2[@]}; i++ )); do
+        v1[i]=0
+    done
+
+    for (( i=0; i<${#v1[@]}; i++ )); do
+
+        # Fill empty positions in v2 with zeros
+        if [[ -z ${v2[i]} ]]; then
+            v2[i]=0
+        fi
+
+        if (( 10#${v1[i]} > 10#${v2[i]} )); then
+            printf ">"
+        fi
+
+        if (( 10#${v1[i]} < 10#${v2[i]} )); then
+            printf "<"
+        fi
+
+    done
+
+    printf "="
+}
+
+# ----------------------------------------------------------------------
+# | Main                                                               |
+# ----------------------------------------------------------------------
+
+main() {
+
+    # Ensure the OS is OS X or Ubuntu
+    check_os || exit;
+
+    # Ensure that the following actions are made
+    # relative to the dotfiles directory root
+    # http://mywiki.wooledge.org/BashFAQ/028
+    cd "$(dirname "${BASH_SOURCE}")";
+
+    source ./bin/utils.sh
+
+    ask_for_sudo
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    print_info "Clone repos"
+    ./bin/clone_repos.sh
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    print_info "Create symbolic links"
+    ./bin/create_symbolic_links.sh
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    print_info "Install Prezto"
+
+    ask_for_confirmation "Do you want to install Prezto?"
+    printf "\n"
+
+    if answer_is_yes; then
+        ./bin/install_prezto.sh
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    print_info "Install applications"
+
+    ask_for_confirmation "Do you want to install the applications/command line tools?"
+    printf "\n"
+
+    if answer_is_yes; then
+        ./bin/install_applications.sh
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    print_info "Restart"
+
+    ask_for_confirmation "Do you want to restart?"
+    printf "\n"
+
+    if answer_is_yes; then
+        ./bin/restart_computer.sh
+    fi
+
+}
+
+main
